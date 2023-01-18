6d1ad3413ff090466c8280e6420bcd99f4d79c65
ARMatthias <122485183+ARMatthias@users.noreply.github.com>
2023-01-18 12:04:41 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,8 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Katie Rae's Number: %d", num);
+            String name = System.getProperty("user.name");
+            return String.format(name + "'s' Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
